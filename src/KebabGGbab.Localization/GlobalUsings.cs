51c10842159e9efee94380e4cd2018544cabccd6
artem <kebabggbab@gmail.com>
2026-05-22 01:55:57 +0700

    chore: unused global namespace removed

--- src/KebabGGbab.Localization/GlobalUsings.cs
+++ src/KebabGGbab.Localization/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Globalization;
-global using System.Threading;
 global using System.Linq;
 
 global using KebabGGbab.Localization.Resources;
