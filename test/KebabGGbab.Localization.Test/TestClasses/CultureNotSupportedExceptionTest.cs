b29fa81891848a1ce0a341a037b33bbe4a324256
artem <kebabggbab@gmail.com>
2026-05-23 02:20:34 +0700

    test: added tests for ResourceNotFoundException and CultureNotSupportedException classes

---
+++ test/KebabGGbab.Localization.Test/TestClasses/CultureNotSupportedExceptionTest.cs
@@ -0,0 +1,130 @@
+﻿using System.Globalization;
+using KebabGGbab.Localization.Exceptions;
+
+namespace KebabGGbab.Localization.Test.TestClasses
+{
+    [TestClass]
+    public sealed class CultureNotSupportedExceptionTest
+    {
+        [TestMethod]
+        public void Ctor_WithoutArgument_CultureIsNull()
+        {
+            CultureNotSupportedException ex = new();
+
+            Assert.IsNull(ex.Culture);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCulture_ParameterSavedInProperty()
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("fr-FR");
+
+            CultureNotSupportedException ex = new(culture);
+
+            Assert.AreEqual(culture, ex.Culture);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCultureIsNull_ParameterSavedInProperty()
+        {
+            CultureInfo? culture = null;
+
+            CultureNotSupportedException ex = new(culture);
+
+            Assert.IsNull(ex.Culture);
+        }
+
+        [TestMethod]
+        public void Ctor_WithMessage_ParameterSavedInProperty()
+        {
+            string message = "exception message";
+
+            CultureNotSupportedException ex = new(message);
+
+            Assert.IsNull(ex.Culture);
+            Assert.AreEqual(message, ex.Message);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCultureAndMessage_ParametersSavedInProperties()
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("fr-FR");
+            string message = "exception message";
+
+            CultureNotSupportedException ex = new(culture, message);
+
+            Assert.AreEqual(culture, ex.Culture);
+            Assert.AreEqual(message, ex.Message);
+        }
+
+        [TestMethod]
+        public void Ctor_WithMessageAndInnerException_ParametersSavedInProperties()
+        {
+            string message = "exception message";
+            Exception innerException = new();
+
+            CultureNotSupportedException ex = new(message, innerException);
+
+            Assert.IsNull(ex.Culture);
+            Assert.AreEqual(message, ex.Message);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCultureAndInnerException_ParametersSavedInProperties()
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("fr-FR");
+            Exception innerException = new();
+
+            CultureNotSupportedException ex = new(culture, innerException);
+
+            Assert.AreEqual(culture, ex.Culture);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCultureAndMessageAndInnerException_ParametersSavedInProperties()
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("fr-FR");
+            string message = "exception message";
+            Exception innerException = new();
+
+            CultureNotSupportedException ex = new(culture, message, innerException);
+
+            Assert.AreEqual(culture, ex.Culture);
+            Assert.AreEqual(message, ex.Message);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void Ctor_WithCultureAndMessageAreNullAndInnerException_ParametersSavedInProperties()
+        {
+            CultureInfo? culture = null;
+            string? message = null;
+            Exception innerException = new();
+
+            CultureNotSupportedException ex = new(culture, message, innerException);
+
+            Assert.AreEqual(culture, ex.Culture);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void ThrowIfCultureNotSupported_IsSupported_NotThrow()
+        {
+            bool isSupported = true;
+
+            CultureNotSupportedException.ThrowIfCultureNotSupported(isSupported, CultureInfo.CurrentCulture);
+        }
+
+        [TestMethod]
+        public void ThrowIfCultureNotSupported_IsNotSupported_Throw()
+        {
+            bool isSupported = false;
+
+            void action() => CultureNotSupportedException.ThrowIfCultureNotSupported(isSupported, CultureInfo.CurrentCulture);
+
+            Assert.ThrowsExactly<CultureNotSupportedException>(action);
+        }
+    }
+}
---
+++ test/KebabGGbab.Localization.Test/TestClasses/ResourceNotFoundExceptionTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Globalization;
+using KebabGGbab.Localization.Exceptions;
+
+namespace KebabGGbab.Localization.Test.TestClasses
+{
+    [TestClass]
+    public sealed class ResourceNotFoundExceptionTest
+    {
+        [TestMethod]
+        public void Ctor_WithoutArguments_KeyIsNull()
+        {
+            ResourceNotFoundException ex = new();
+
+            Assert.IsNull(ex.Key);
+        }
+
+        [TestMethod]
+        public void Ctor_WithMessage_ParameterSavedInProperty()
+        {
+            string message = "exception message";
+
+            ResourceNotFoundException ex = new(message);
+
+            Assert.IsNull(ex.Key);
+            Assert.AreEqual(message, ex.Message);
+        }
+
+        [TestMethod]
+        public void Ctor_WithKeyAndMessage_ParametersSavedInProperties()
+        {
+            string key = "localizaion key";
+            string message = "exception message";
+
+            ResourceNotFoundException ex = new(key, message);
+
+            Assert.AreEqual(key, ex.Key);
+            Assert.AreEqual(message, ex.Message);
+        }
+
+        [TestMethod]
+        public void Ctor_WithMessageAndInnerException_ParametersSavedInProperties()
+        {
+            string message = "exception message";
+            Exception innerException = new();
+
+            CultureNotSupportedException ex = new(message, innerException);
+
+            Assert.IsNull(ex.Culture);
+            Assert.AreEqual(message, ex.Message);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void Ctor_WithKeyAndMessageAndInnerException_ParametersSavedInProperties()
+        {
+            string key = "localizaion key";
+            string message = "exception message";
+            Exception innerException = new();
+
+            ResourceNotFoundException ex = new(key, message, innerException);
+
+            Assert.AreEqual(key, ex.Key);
+            Assert.AreEqual(message, ex.Message);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void Ctor_WithKeyAndMessageAreNullAndInnerException_ParametersSavedInProperties()
+        {
+            string? key = null;
+            string? message = null;
+            Exception innerException = new();
+
+            ResourceNotFoundException ex = new(key, message, innerException);
+
+            Assert.AreEqual(key, ex.Key);
+            Assert.AreEqual(innerException, ex.InnerException);
+        }
+
+        [TestMethod]
+        public void ThrowIfResourceNotFound_IsFounded_NotThrow()
+        {
+            bool isFounded = true;
+
+            ResourceNotFoundException.ThrowIfResourceNotFound(isFounded, "localizaion key");
+        }
+
+        [TestMethod]
+        public void ThrowIfResourceNotFound_IsNotSupported_Throw()
+        {
+            bool isFounded = false;
+
+            void action() => ResourceNotFoundException.ThrowIfResourceNotFound(isFounded, "localizaion key");
+
+            Assert.ThrowsExactly<ResourceNotFoundException>(action);
+        }
+    }
+}
