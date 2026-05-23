b2eb307314d1af8729ad5b95b135c3acf6bd8662
artem <kebabggbab@gmail.com>
2026-05-24 00:22:08 +0700

    test: added mocks for ICultureService

---
+++ test/KebabGGbab.Localization.Test/Mocks/MocksCultureService.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using KebabGGbab.Localization.CultureService;
+
+namespace KebabGGbab.Localization.Test.Mocks
+{
+    internal sealed class MockCultureService : ICultureService
+    {
+        public CultureInfo CurrentUICulture { get; private set; } = CultureInfo.GetCultureInfo("en-US");
+
+        public bool ChangeCurrentUICulture(CultureInfo newCulture)
+        {
+            CurrentUICulture = newCulture;
+
+            return true;
+        }
+    }
+}
