d8e55c59477dd6df4d639002029aa134183de063
artem <kebabggbab@gmail.com>
2026-05-26 04:02:44 +0700

    samples(wpf): added sample for wpf

---
+++ samples/KebabGGbab.Localization.Samples.WPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using KebabGGbab.Localization.Samples.WPF.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KebabGGbab.Localization.Samples.WPF
+{
+    public partial class App : Application
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddLocalizationWithResx();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _serviceProvider.GetRequiredService<MainWindow>().Show();
+        }
+    }
+}
---
+++ samples/KebabGGbab.Localization.Samples.WPF/Extensions/LocalizationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using KebabGGbab.Localization.Samples.WPF.Resources;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KebabGGbab.Localization.Samples.WPF.Extensions
+{
+    public static class LocalizationExtensions
+    {
+        public static IServiceCollection AddLocalizationWithResx(this ServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddLocalization();
+            services.AddResxLocalization(StringsUI.ResourceManager, [CultureInfo.GetCultureInfo("ru-RU"), CultureInfo.GetCultureInfo("en-US")]);
+
+            return services;
+        }
+    }
+}
