f4065d211b65745eda4ee5de92f3182dd5b02db5
artem <kebabggbab@gmail.com>
2026-05-26 23:26:13 +0700

    feat(avaloniaui): import legacy code for AvaloniaUI

---
+++ src/KebabGGbab.Localization.AvaloniaUI/Localization.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+
+namespace KebabGGbab.Avalonia.Extensions.Services.Localization
+{
+    public class Localization : MarkupExtension
+    {
+        public required string Key { get; set; }
+
+        public string[]? Arguments { get; set; }
+
+        public Localization()
+        {
+        }
+
+        public Localization(string key)
+        {
+            Key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            IRootObjectProvider? root = (IRootObjectProvider?)serviceProvider.GetService(typeof(IRootObjectProvider));
+            ArgumentNullException.ThrowIfNull(root, nameof(root));
+
+            TopLevel topLevel = (TopLevel)root.RootObject;
+            LocalizationListener listener = new(topLevel, Key, Arguments);
+            Binding binding = new()
+            {
+                Source = listener,
+                Path = nameof(LocalizationListener.Value),
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+            
+            return binding;
+        }
+    }
+}
---
+++ src/KebabGGbab.Localization.AvaloniaUI/LocalizationListener.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Text;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using KebabGGbab.Avalonia.Extensions.Resources.Strings;
+using KebabGGbab.Localization;
+using KebabGGbab.Localization.Abstractions;
+
+namespace KebabGGbab.Avalonia.Extensions.Services.Localization
+{
+    public class LocalizationListener : ObservableObject
+    {
+        private static readonly CompositeFormat _resourcePlaceholder = CompositeFormat.Parse(S_LocalizationService.ResourcePlaceholder);
+        
+        private readonly TopLevel _root;
+
+        public string Key { get; }
+
+        public string[]? Arguments { get; }
+
+        public object? Value 
+        {
+            get => field;
+            private set => SetProperty(ref field, value);
+        }
+
+        public LocalizationListener(TopLevel root, string key, string[]? arguments = null)
+        {
+            Key = key;
+            Arguments = arguments;
+            LocalizeValue();
+            _root = root;
+            _root.Closed += Root_Closed;
+            LocalizationManager.Instance.CurrentUICultureChanged += OnCurrentUICultureChanged;
+        }
+
+        private void OnCurrentUICultureChanged(ILocalizationManager sender, CurrentUICultureChangedEventArgs e)
+        {
+            LocalizeValue();
+        }
+
+        private void LocalizeValue()
+        {
+            try
+            {
+                object value = LocalizationManager.Instance.Localize(Key);
+
+                if (value is string str && Arguments != null)
+                {
+                    Value = string.Format(str, Arguments);
+                }
+                else
+                {
+                    Value = value;
+                }
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                Value = string.Format(CultureInfo.InvariantCulture, _resourcePlaceholder, ex.Key);
+            }
+        }
+
+        private void Root_Closed(object? sender, EventArgs e)
+        {
+            _root.Closed -= Root_Closed;
+            LocalizationManager.Instance.CurrentUICultureChanged -= OnCurrentUICultureChanged;
+        }
+    }
+}
