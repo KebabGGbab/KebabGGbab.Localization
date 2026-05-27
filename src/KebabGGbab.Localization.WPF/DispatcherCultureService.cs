5e7e2ccfc9a43bd9af1fc77e838bcd5ff3198499
artem <kebabggbab@gmail.com>
2026-05-28 02:23:22 +0700

    feat(wpf): add DispatcherCultureService class implementing ICultureService

---
+++ src/KebabGGbab.Localization.WPF/DispatcherCultureService.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Threading;
+using KebabGGbab.Localization.CultureService;
+
+namespace KebabGGbab.Localization.WPF
+{
+    public sealed class DispatcherCultureService : ICultureService
+    {
+        public CultureInfo CurrentUICulture
+        {
+            get => Dispatcher.CurrentDispatcher.Thread.CurrentUICulture;
+            private set => Dispatcher.CurrentDispatcher.Thread.CurrentUICulture = value;
+        }
+
+        public bool ChangeCurrentUICulture(CultureInfo newCulture)
+        {
+            if (CurrentUICulture == newCulture)
+            {
+                return false;
+            }
+
+            CurrentUICulture = newCulture;
+
+            return true;
+        }
+    }
+}
